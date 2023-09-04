8c07c40bbe48adbbece9c4c2ff80960d675b2fe9
j <jacobsmithsinbox@gmail.com>
2023-09-03 19:39:42 -0600

    Update crypto.h

--- include/crypto/crypto.h
+++ include/crypto/crypto.h
@@ -19,13 +19,26 @@
 #define DLLEXPORT
 #endif
 
+// Type definitions
+typedef unsigned long long(*crypto_hash_function_64_t)( const void *const k, size_t l );
+
 // Function declarations
 /**!
- * Hash l bytes of pointer k using the MurMur64 hash function
+ * Hash data using the MurMur64 hash function
  * 
  * @param k pointer to data to be hashed
  * @param l number of bytes to hash
  * 
- * @return the MurMur64 hash
+ * @return the 64-bit hash on success, 0 on error
 */
-DLLEXPORT unsigned long long crypto_mmh64 ( const void* const k, size_t l )
+DLLEXPORT unsigned long long crypto_mmh64 ( const void* const k, size_t l );
+
+/**!
+ * Hash data using the xxHash64 hash function
+ * 
+ * @param k pointer to data to be hashed
+ * @param l number of bytes to hash
+ * 
+ * @return the 64-bit hash on success, 0 on error
+*/
+DLLEXPORT unsigned long long crypto_xxh64 ( const void *const k, size_t l ); 
